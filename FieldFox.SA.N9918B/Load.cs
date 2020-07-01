16cec4c7af274555e1864e62f2e57e09b66461fa
Tarang Kalaria <41413595+tarang1606@users.noreply.github.com>
2020-07-01 16:55:35 +0530

    Add files via upload

---
+++ FieldFox.SA.N9918B/Load.cs
@@ -0,0 +1,137 @@
+﻿// Author: MyName
+// Copyright:   Copyright 2020 Keysight Technologies
+//              You have a royalty-free right to use, modify, reproduce and distribute
+//              the sample application files (and/or any modified version) in any way
+//              you find useful, provided that you agree that Keysight Technologies has no
+//              warranty, obligations or liability for any sample application files.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using OpenTap;
+
+namespace FieldFox.SA.N9918B
+{
+	[Display("Load", Group: "FieldFox.SA.N9918B", Description: "Insert a description here")]
+	public class Load : TestStep
+	{
+        #region Settings
+
+        [Display("Instrument", Group: "FieldFox.NA.N9918B", Description: "Insert a description here")]
+        public N9918B MyInst { get; set; }
+
+        private string _LoadAntenna = "MyAntenna";
+
+        [DisplayAttribute("LoadAntenna", "", "Input Parameters", 2)]
+        public string LoadAntenna
+        {
+            get
+            {
+                return this._LoadAntenna;
+            }
+            set
+            {
+                this._LoadAntenna = value;
+            }
+        }
+
+        private string _LoadMode = "INTernal";
+
+        [DisplayAttribute("LoadMode", "{ INTernal, USB, SD }", "Input Parameters", 2)]
+        public string LoadMode
+        {
+            get
+            {
+                return this._LoadMode;
+            }
+            set
+            {
+                this._LoadMode = value;
+            }
+        }
+
+        private string _LoadCable = "MyCable";
+
+        [DisplayAttribute("LoadCable", "", "Input Parameters", 2)]
+        public string LoadCable
+        {
+            get
+            {
+                return this._LoadCable;
+            }
+            set
+            {
+                this._LoadCable = value;
+            }
+        }
+
+        private string _LoadSourceAntenna = "MyAntenna.ant";
+
+        [DisplayAttribute("LoadSourceAntenna", "", "Input Parameters", 2)]
+        public string LoadSourceAntenna
+        {
+            get
+            {
+                return this._LoadSourceAntenna;
+            }
+            set
+            {
+                this._LoadSourceAntenna = value;
+            }
+        }
+
+        private string _LoadSourceCable = "MyCable.csv";
+
+        [DisplayAttribute("LoadSourceCable", "", "Input Parameters", 2)]
+        public string LoadSourceCable
+        {
+            get
+            {
+                return this._LoadSourceCable;
+            }
+            set
+            {
+                this._LoadSourceCable = value;
+            }
+        }
+
+        private string _LoadState = "MyState.sta";
+
+        [DisplayAttribute("LoadState", "", "Input Parameters", 2)]
+        public string LoadState
+        {
+            get
+            {
+                return this._LoadState;
+            }
+            set
+            {
+                this._LoadState = value;
+            }
+        }
+
+
+        #endregion
+
+        public Load()
+		{
+			// ToDo: Set default values for properties / settings.
+		}
+
+		public override void Run()
+		{
+			// ToDo: Add test case code.
+			RunChildSteps(); //If the step supports child steps.
+
+            MyInst.ScpiCommand(":MMEMory:LOAD:ANTenna {0},{1}", LoadAntenna, LoadMode);
+            MyInst.ScpiCommand(":MMEMory:LOAD:CABLe {0},{1}", LoadCable, LoadMode);
+            MyInst.ScpiCommand(":MMEMory:LOAD:SANTenna {0},{1}", LoadSourceAntenna, LoadMode);
+            MyInst.ScpiCommand(":MMEMory:LOAD:SCABle {0},{1}", LoadSourceCable, LoadMode);
+            MyInst.ScpiCommand(":MMEMory:LOAD:STATe {0}", LoadState);
+            // If no verdict is used, the verdict will default to NotSet.
+            // You can change the verdict using UpgradeVerdict() as shown below.
+            // UpgradeVerdict(Verdict.Pass);
+        }
+	}
+}
---
+++ FieldFox.SA.N9918B/Memory.cs
@@ -0,0 +1,182 @@
+﻿// Author: MyName
+// Copyright:   Copyright 2020 Keysight Technologies
+//              You have a royalty-free right to use, modify, reproduce and distribute
+//              the sample application files (and/or any modified version) in any way
+//              you find useful, provided that you agree that Keysight Technologies has no
+//              warranty, obligations or liability for any sample application files.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using OpenTap;
+
+namespace FieldFox.SA.N9918B
+{
+	[Display("Memory", Group: "FieldFox.SA.N9918B", Description: "Insert a description here")]
+	public class Memory : TestStep
+	{
+        #region Settings
+
+        [Display("Instrument", Group: "FieldFox.NA.N9918B", Description: "Insert a description here")]
+        public N9918B MyInst { get; set; }
+
+        private string _CDirectory = "C:\\temp\\";
+
+        [DisplayAttribute("CDirectory", "", "Input Parameters", 2)]
+        public string CDirectory
+        {
+            get
+            {
+                return this._CDirectory;
+            }
+            set
+            {
+                this._CDirectory = value;
+            }
+        }
+
+        private string _SourceFile = "source";
+
+        [DisplayAttribute("SourceFile", "", "Input Parameters", 2)]
+        public string SourceFile
+        {
+            get
+            {
+                return this._SourceFile;
+            }
+            set
+            {
+                this._SourceFile = value;
+            }
+        }
+
+        private string _DestinationFile = "destination";
+
+        [DisplayAttribute("DestinationFile", "", "Input Parameters", 2)]
+        public string DestinationFile
+        {
+            get
+            {
+                return this._DestinationFile;
+            }
+            set
+            {
+                this._DestinationFile = value;
+            }
+        }
+
+        private string _Delete = "file";
+
+        [DisplayAttribute("Delete", "", "Input Parameters", 2)]
+        public string Delete
+        {
+            get
+            {
+                return this._Delete;
+            }
+            set
+            {
+                this._Delete = value;
+            }
+        }
+
+        private string _MDirectory = "[USBDISK]:\\MyFolder";
+
+        [DisplayAttribute("MDirectory", "", "Input Parameters", 2)]
+        public string MDirectory
+        {
+            get
+            {
+                return this._MDirectory;
+            }
+            set
+            {
+                this._MDirectory = value;
+            }
+        }
+
+        private string _FromFile = "file1";
+
+        [DisplayAttribute("FromFile", "", "Input Parameters", 2)]
+        public string FromFile
+        {
+            get
+            {
+                return this._FromFile;
+            }
+            set
+            {
+                this._FromFile = value;
+            }
+        }
+
+        private string _ToFile = "file2";
+
+        [DisplayAttribute("ToFile", "", "Input Parameters", 2)]
+        public string ToFile
+        {
+            get
+            {
+                return this._ToFile;
+            }
+            set
+            {
+                this._ToFile = value;
+            }
+        }
+
+        private string _RDirectory = "C:\\temp\\";
+
+        [DisplayAttribute("RDirectory", "", "Input Parameters", 2)]
+        public string RDirectory
+        {
+            get
+            {
+                return this._RDirectory;
+            }
+            set
+            {
+                this._RDirectory = value;
+            }
+        }
+
+        private string _RDirectoryRecursive = "recursive";
+
+        [DisplayAttribute("RDirectoryRecursive", "", "Input Parameters", 2)]
+        public string RDirectoryRecursive
+        {
+            get
+            {
+                return this._RDirectoryRecursive;
+            }
+            set
+            {
+                this._RDirectoryRecursive = value;
+            }
+        }
+
+        #endregion
+
+        public Memory()
+		{
+			// ToDo: Set default values for properties / settings.
+		}
+
+		public override void Run()
+		{
+			// ToDo: Add test case code.
+			RunChildSteps(); //If the step supports child steps.
+
+            MyInst.ScpiCommand(":MMEMory:CDIRectory {0}", CDirectory);
+            MyInst.ScpiCommand(":MMEMory:COPY {0},{1}", SourceFile, DestinationFile);
+            MyInst.ScpiCommand(":MMEMory:DELete {0}", Delete);
+            MyInst.ScpiCommand(":MMEMory:MDIRectory {0}", MDirectory);
+            MyInst.ScpiCommand(":MMEMory:MOVE {0},{1}", FromFile, ToFile);
+            MyInst.ScpiCommand(":MMEMory:RDIRectory {0},{1}", RDirectory, RDirectoryRecursive);
+            // If no verdict is used, the verdict will default to NotSet.
+            // You can change the verdict using UpgradeVerdict() as shown below.
+            // UpgradeVerdict(Verdict.Pass);
+        }
+	}
+}
